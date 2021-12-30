c871dcd0f53c5e9f4bae5a8b19c1eb50ba587ea7
I-the-Creator <andrey.burluckiy@gmail.com>
2021-12-30 17:53:32 +0100

    initial commit without logic

---
+++ main.js
@@ -0,0 +1,13 @@
+const engWord = document.getElementById('eng'),
+      rusWord = document.getElementById('erus'),
+      inputs = document.getElementsByClassName('input'),
+      addButton = document.getElementsById('add-word-btn'),
+      table = document.getElementById('table');
+
+let words;
+let deleteBtn;
+
+// функция конструктор для создании языковой пары 
+const CreateWord = (english, russian) => {
+
+}
