f7f511921093ceeed649f4bd5ebb421d0b932907
I-the-Creator <andrey.burluckiy@gmail.com>
2021-12-31 11:54:49 +0100

    logic add

--- main.js
+++ main.js
@@ -7,8 +7,44 @@ const engWord = document.getElementById('eng'),
 let words;  // переменная для хранения массива словарных пар
 let deleteBtn;
 
-// проверяем есть ли сохраненные данные в localStorage
+// проверяем есть ли сохраненные данные в localStorage, если нет то присваиваем words пустой массив
 localStorage.length < 1 ? words = [] : words = JSON.parse(localStorage.getItem('words'));
+
+const addEventDelete = () => {
+    // провераяем не пустой ли массив 'words'
+    if(words.length > 0) {
+        /* только после проверки присваиваем переменной значение, 
+        если сделать это раньше то может быть ошибка при пустом массиве */
+        deleteBtn = document.querySelectorAll('.btn-delete');
+        for(let btn of deleteBtn) {  // пробегаемся по всем кнопкам
+            btn.addEventListener('click', e => {  // добавляем прослушку
+                deleteWord(e);
+            })
+        }
+    }
+}
+
+const addWordToTable = index => {  // words[index] - словарная пара - элемент массива
+    table.innerHTML += `
+        <tr class="tr-${index + 1}">
+            <td class="eng-word">${words[index].english}</td>    
+            <td class="rus-word">${words[index].russian}</td>
+            <td class="">
+                <button class="btn-delete"></button>
+            </td>
+        </tr>
+    ` 
+    addEventDelete();  // добавляем прослушку к новым созданным строкам
+}
+
+/* при перезагрузке страницы парсим данные хранящиеся в localStorage
+'element' - элемент массива - словарная пара
+'i' - индекс элемента в массиве 'words' */
+words.forEach((element, i) => {
+    addWordToTable(i);
+});
+
+
 
 addButton.addEventListener('click', () => {
     if(
@@ -26,6 +62,9 @@ addButton.addEventListener('click', () => {
         }
         words.push(new CreateWord(engWord.value, rusWord.value));
         localStorage.setItem('words', JSON.stringify(words));
+        addWordToTable(words.length - 1);  // передаем индекс элемента массива
+        engWord.value = null;   // очистка инпута
+        rusWord.value = null;  // очистка инпута
     }
 })
 
@@ -34,3 +73,21 @@ function CreateWord (english, russian) {
     this.english = english;
     this.russian = russian;
 }
+
+const deleteWord = event => {   // удаление строки в которой кликнули кнопку 'delete'
+    /*получаем доступ к ряду <tr> в котором мы кликнули кнопку
+    и с помощтю свойства rowIndex - получаем индекс этого ряда - нумерация с нуля */
+    const rowIndex = event.target.parentNode.parentNode.rowIndex;
+        console.log(rowIndex);
+    event.target.parentNode.parentNode.parentNode.remove();      // удаление строки из верстки
+
+    // удаление элемента из массива
+    words.splice(rowIndex, 1); // удаляем один элемент начиная с индекса равного rowIndex
+    // перезаписываем localstorage 
+    localStorage.removeItem('words');
+    localStorage.setItem('words',JSON.stringify(words));
+}
+
+addEventDelete();
+
+// document.querySelector('.tr-1').parentNode.remove();
